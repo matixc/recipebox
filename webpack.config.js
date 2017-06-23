f6130c4754aa33434cdf8838214509f1eb3518ed
Mateusz Dziezok <mateusz.dziezok@gmail.com>
2017-06-23 09:18:08 +0200

    add css-loader & all nesesery changes

--- webpack.config.js
+++ webpack.config.js
@@ -11,13 +11,19 @@ module.exports = {
     },
 
     module: {
-        loaders: [{
+        loaders: [
+        {
             test: /\.js$/,
             exclude: /(node_modules)/,
             loader: "babel-loader",
             query: {
                 presets: ["es2015", "react"]
             }
-        }]
+        },
+        {
+            test: /\.css$/,
+            loader: "style-loader!css-loader"
+        }
+        ]
     }
 };
